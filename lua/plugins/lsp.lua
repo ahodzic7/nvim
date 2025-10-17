b17723f46a70abe86f68a48f8f1d573042d8410a
ahodzic7 <adish96@live.com>
2025-10-17 11:29:10 +0200

    automated dev commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,56 +3,63 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/nvim-cmp", -- Optional: Autocompletion support
-        "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-        "L3MON4D3/LuaSnip", -- Optional: Snippets support
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
     },
 
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "clangd", "cmake"
+                "clangd", "cmake", "omnisharp"
             },
             automatic_installation = true,
         })
 
-        -- 3. LSP Configuration for clangd
         local lspconfig = require("lspconfig")
-
-        -- Optional: Add completion capabilities if nvim-cmp is installed
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+        -- Define on_attach ONCE to use for all servers
+        local on_attach = function(client, bufnr)
+            local opts = { buffer = bufnr }
+            local keymap = vim.keymap.set
+            keymap("n", "gd", vim.lsp.buf.definition, opts)
+            keymap("n", "K", vim.lsp.buf.hover, opts)
+            keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
+            keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+            keymap("n", "]d", vim.diagnostic.goto_next, opts)
+        end
+
+        -- C++ LSP setup
         lspconfig.clangd.setup({
             cmd = { "clangd" },
-            filetypes = { "c", "cpp", "objc", "objcpp" }, -- Filetypes handled
+            filetypes = { "c", "cpp", "objc", "objcpp" },
             root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-            capabilities = capabilities, -- Autocompletion capabilities
-            on_attach = function(client, bufnr)
-                -- Key mappings for LSP functions
-                local opts = { buffer = bufnr }
-                local keymap = vim.keymap.set
-
-                keymap("n", "gd", vim.lsp.buf.definition, opts)
-                keymap("n", "K", vim.lsp.buf.hover, opts)
-                keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-                keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
-                keymap("n", "[d", vim.diagnostic.goto_prev, opts)
-                keymap("n", "]d", vim.diagnostic.goto_next, opts)
-            end,
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
 
-        -- 4. Autocompletion setup (optional)
+        -- C# LSP setup (OmniSharp via Mason for Linux/WSL)
+        lspconfig.omnisharp.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj"),
+            cmd = { vim.fn.stdpath("data") .. "/mason/packages/omnisharp/OmniSharp", "-lsp" },
+        })
+
+        -- Autocompletion setup
         local cmp = require("cmp")
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
-                ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion menu
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
             sources = {
-                { name = "nvim_lsp" }, -- Use LSP as completion source
-                { name = "buffer" },   -- Buffer completions
+                { name = "nvim_lsp" },
+                { name = "buffer" },
             },
         })
     end,
