7481e9d6b9d9da0c7b9e456b43fdc4218691748a
ahodzic7 <adish96@live.com>
2025-10-22 11:09:09 +0200

    automated dev commit

--- lua/theaho/remap.lua
+++ lua/theaho/remap.lua
@@ -31,3 +31,8 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 --vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- QOL for saving etc.
+vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>x', ':wq<CR>', { noremap = true, silent = true }
