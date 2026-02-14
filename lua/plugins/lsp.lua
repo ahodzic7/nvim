1380873b9e70765c45e9051ec256c4545131a6a6
ahodzic7 <adish96@live.com>
2026-02-14 14:51:29 +0100

    automated dev commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,11 +31,26 @@ return {
             keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
             keymap("n", "[d", vim.diagnostic.goto_prev, opts)
             keymap("n", "]d", vim.diagnostic.goto_next, opts)
+            if client.server_capabilities.documentFormattingProvider then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({
+                            async = false,
+                            bufnr = bufnr,
+                        })
+                    end,
+                })
+            end
         end
 
         -- C++ LSP setup
         lspconfig.clangd.setup({
-            cmd = { "clangd" },
+            cmd = { "clangd",
+                "--background-index",
+                "--clang-tidy",
+                "--completion-style=detailed",
+                "--header-insertion=iwyu"},
             filetypes = { "c", "cpp", "objc", "objcpp" },
             root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
             capabilities = capabilities,
@@ -79,11 +94,11 @@ return {
             capabilities = capabilities,
             on_attach = on_attach,
             root_dir = lspconfig.util.root_pattern(
-            "pyproject.toml",
-            "setup.py",
-            "setup.cfg",
-            "requirements.txt",
-            ".git"
+                "pyproject.toml",
+                "setup.py",
+                "setup.cfg",
+                "requirements.txt",
+                ".git"
             ),
             settings = {
                 python = {
