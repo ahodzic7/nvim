9cf013f21a0546b3d81110f0615934e40081386b
ahodzic7 <adish96@live.com>
2026-02-15 13:58:44 +0100

    automated dev commit

--- lua/theaho/set.lua
+++ lua/theaho/set.lua
@@ -1,9 +1,15 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+--cpp:
+vim.opt.colorcolumn = "80"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+--vim.opt.tabstop = 4
+--vim.opt.softtabstop = 4
+--vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -25,8 +31,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
---vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
