e55b1d5e21ccb675c6a1a2119b21654dd5c37a30
AHO <aho@eiva.com>
2026-04-01 11:57:44 +0200

    automated dev commit

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,13 +10,6 @@ return {
     },
 
     config = function()
-        vim.g.copilot_no_tab_map = true
-
-        vim.keymap.set("i", "<C-;>", 'copilot#Accept("\\<CR>")', {
-            expr = true,
-            silent = true,
-        })
-
         local cmp = require("cmp")
         local luasnip = require("luasnip")
 
